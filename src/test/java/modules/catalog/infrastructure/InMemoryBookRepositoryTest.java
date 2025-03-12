f824311ac16f4f52bb1c6f583c650d6fcb945166
Daniele Rossi <drossi@redhat.com>
2025-03-12 11:06:57 +0100

    feat: add InMemory book repository

---
+++ src/test/java/modules/catalog/infrastructure/InMemoryBookRepositoryTest.java
@@ -0,0 +1,57 @@
+package modules.catalog.infrastructure;
+
+import modules.catalog.domain.Book;
+import modules.catalog.utils.CatalogTestUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InMemoryBookRepositoryTest {
+
+    private InMemoryBookRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = new InMemoryBookRepository();
+    }
+
+    @Test
+    void saveAndFindByIdSuccessful() {
+        Book bookToSave = CatalogTestUtils.createValidBook();
+        Book savedBook = repository.save(bookToSave);
+        Optional<Book> retrievedBook = repository.findById(savedBook.getBookId());
+        assertTrue(retrievedBook.isPresent());
+        assertEquals(savedBook.getBookId(), retrievedBook.get().getBookId());
+    }
+
+    @Test
+    void findByIdNotFound() {
+        Optional<Book> retrievedBook = repository.findById(UUID.randomUUID());
+        assertFalse(retrievedBook.isPresent());
+    }
+
+    @Test
+    void findAll_BooksExist() {
+        repository.save(CatalogTestUtils.createValidBook());
+        repository.save(CatalogTestUtils.createValidBook());
+        List<Book> allBooks = repository.findAll();
+        assertEquals(2, allBooks.size());
+    }
+
+    @Test
+    void findAll_NoBooks() {
+        List<Book> allBooks = repository.findAll();
+        assertTrue(allBooks.isEmpty());
+    }
+
+    @Test
+    void deleteByIdSuccessful() {
+        Book bookToDelete = CatalogTestUtils.createValidBook();
+        Book savedBook = repository.save(bookToDelete);
+        repository.deleteById(savedBook.getBookId());
+        Optional<Book> deletedBook = repository.findById(savedBook.getBookId());
+        assertFalse(deletedBook.isPresent());
+    }
+}
