ec85cbf8959cf373c95254b5859d5c73454d435e
Daniele Rossi <drossi@redhat.com>
2025-03-31 09:38:11 +0200

    feat: add security

--- src/main/java/modules/user/web/controllers/UserController.java
+++ src/main/java/modules/user/web/controllers/UserController.java
@@ -1,10 +1,12 @@
 package modules.user.web.controllers;
 
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import modules.user.domain.User;
@@ -13,22 +15,41 @@ import modules.user.web.dto.UserResponseDTO;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import io.quarkus.security.Authenticated;
+import jakarta.ws.rs.core.SecurityContext;
+
 @Path("/api/v1/users")
 @Produces(MediaType.APPLICATION_JSON)
+@Authenticated
 public class UserController {
 
     @Inject
     UserService userService;
 
+    @Inject
+    JsonWebToken jwt;
+
+    @Context
+    SecurityContext ctx;
+
     @GET
     @Path("/{userId}")
+    @RolesAllowed({"user", "admin"})
     public Response getUserById(@PathParam("userId") UUID userId) {
-        Optional<User> userOptional = userService.findUserProfileById(userId);
-        if (userOptional.isPresent()) {
-            UserResponseDTO responseDTO = mapToUserResponseDTO(userOptional.get());
-            return Response.ok(responseDTO).build();
+        UUID authenticatedUserId = UUID.fromString(jwt.getClaim("sub"));
+
+        if ((authenticatedUserId != null && (authenticatedUserId.equals(userId)) || ctx.isUserInRole("admin"))) {
+            Optional<User> userOptional = userService.findUserProfileById(userId);
+            if (userOptional.isPresent()) {
+                UserResponseDTO responseDTO = mapToUserResponseDTO(userOptional.get());
+                return Response.ok(responseDTO).build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
         } else {
-            return Response.status(Response.Status.NOT_FOUND).build();
+            return Response.status(Response.Status.FORBIDDEN).entity("Access denied.").build();
         }
     }
 
--- src/test/java/modules/user/web/controllers/UserControllerIntegrationTest.java
+++ src/test/java/modules/user/web/controllers/UserControllerIntegrationTest.java
@@ -1,6 +1,8 @@
 package modules.user.web.controllers;
 
+import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.keycloak.client.KeycloakTestClient;
 import jakarta.inject.Inject;
 import modules.user.domain.User;
 import modules.user.domain.UserImpl;
@@ -13,50 +15,89 @@ import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.notNullValue;
 
 @QuarkusTest
+@TestHTTPEndpoint(UserController.class)
 public class UserControllerIntegrationTest {
 
     @Inject
     UserServiceImpl userService;
+    KeycloakTestClient keycloakClient = new KeycloakTestClient();
 
-    private UUID testUserId;
+    // TODO: This info should be fetched from config/quarkus-realm.json
+    private final User alice = new UserImpl.UserBuilder()
+    .userId(UUID.fromString( "eb4123a3-b722-4798-9af5-8957f823657a"))
+    .firstName("Alice")
+    .lastName("Silverstone")
+    .username("alice")
+    .email("asilverstone@test.com")
+    .build();
+    private final User admin = new UserImpl.UserBuilder()
+    .userId(UUID.fromString("af134cab-f41c-4675-b141-205f975db679"))
+    .firstName("Bruce")
+    .lastName("Wayne")
+    .username("admin")
+    .email("bwayne@test.com")
+    .build();
 
     @BeforeEach
     void setUp() {
-        User testUser = new UserImpl.UserBuilder()
-                .userId(UUID.randomUUID())
-                .firstName("Integration")
-                .lastName("Test")
-                .username("integration_test")
-                .email("integration@test.com")
-                .build();
-        userService.createUserProfile(testUser);
-        testUserId = testUser.getUserId();
+        userService.createUserProfile(alice);
+        userService.createUserProfile(admin);
     }
 
     @Test
-    void testGetUserByIdExistingIdShouldReturnOkAndUser() {
+    void testUserCanAccessHisInformations() {
         given()
-                .pathParam("userId", testUserId)
-                .when().get("/api/v1/users/{userId}")
+                .auth().oauth2(getAccessToken(alice.getUsername()))
+                .pathParam("userId", alice.getUserId().toString())
+                .when().get("/{userId}")
                 .then()
                 .statusCode(200)
-                .body("userId", notNullValue())
-                .body("firstName", equalTo("Integration"))
-                .body("lastName", equalTo("Test"))
-                .body("username", equalTo("integration_test"))
-                .body("email", equalTo("integration@test.com"));
+                .body("userId", equalTo(alice.getUserId().toString()))
+                .body("firstName", equalTo(alice.getFirstName()))
+                .body("lastName", equalTo(alice.getLastName()))
+                .body("username", equalTo(alice.getUsername()))
+                .body("email", equalTo(alice.getEmail()));
     }
 
     @Test
-    void testGetUserByIdNonExistingIdShouldReturnNotFound() {
-        UUID nonExistingId = UUID.randomUUID();
+    void testAdminCanAccessOthersInformations() {
         given()
-                .pathParam("userId", nonExistingId)
-                .when().get("/api/v1/users/{userId}")
+                .auth().oauth2(getAccessToken(admin.getUsername()))
+                .pathParam("userId", alice.getUserId())
+                .when().get("/{userId}")
+                .then()
+                .statusCode(200)
+                .body("userId", equalTo(alice.getUserId().toString()))
+                .body("firstName", equalTo(alice.getFirstName()))
+                .body("lastName", equalTo(alice.getLastName()))
+                .body("username", equalTo(alice.getUsername()))
+                .body("email", equalTo(alice.getEmail()));
+    }
+
+    @Test
+    void testUserCannotAccessOthersInformations() {
+        given()
+                .auth().oauth2(getAccessToken(alice.getUsername()))
+                .pathParam("userId", admin.getUserId())
+                .when().get("/{userId}")
+                .then()
+                .statusCode(403);
+    }
+
+    @Test
+    void testUserNotFound() {
+        given()
+                .auth().oauth2(getAccessToken(admin.getUsername()))
+                .pathParam("userId", UUID.randomUUID())
+                .when().get("/{userId}")
                 .then()
                 .statusCode(404);
+    }
+
+
+    protected String getAccessToken(String userName) {
+        return keycloakClient.getAccessToken(userName);
     }
 }
