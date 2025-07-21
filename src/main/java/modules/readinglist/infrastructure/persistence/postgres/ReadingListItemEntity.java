b5448f62a54443cfbfc801888daff5fdda6ebf87
Daniele Rossi <drossi@redhat.com>
2025-07-21 14:26:46 +0200

    chore: cleanup

--- src/main/java/modules/readinglist/infrastructure/persistence/postgres/ReadingListItemEntity.java
+++ src/main/java/modules/readinglist/infrastructure/persistence/postgres/ReadingListItemEntity.java
@@ -1,5 +1,6 @@
 package modules.readinglist.infrastructure.persistence.postgres;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -8,11 +9,13 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Table(name = "reading_list_items")
 @Data
-public class ReadingListItemEntity {
+@EqualsAndHashCode(callSuper = false)
+public class ReadingListItemEntity extends PanacheEntityBase {
 
     @EmbeddedId
     private ReadingListItemId id;
