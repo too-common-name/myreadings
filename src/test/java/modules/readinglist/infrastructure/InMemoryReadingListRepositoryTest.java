83681d2f946f4bcca85cdd8eb43ed56c53bfdd31
Daniele Rossi <drossi@redhat.com>
2025-08-12 12:10:17 +0200

    fix: jpa tests isolation

--- src/test/java/modules/readinglist/infrastructure/InMemoryReadingListRepositoryTest.java
+++ src/test/java/modules/readinglist/infrastructure/InMemoryReadingListRepositoryTest.java
@@ -2,9 +2,11 @@ package modules.readinglist.infrastructure;
 
 import common.InMemoryRepositoryTestProfile;
 import io.quarkus.test.junit.TestProfile;
+import modules.catalog.core.domain.Book;
 import modules.catalog.utils.CatalogTestUtils;
+import modules.readinglist.core.usecases.repositories.ReadingListRepository;
 import modules.readinglist.infrastructure.persistence.in_memory.InMemoryReadingListRepository;
-import modules.readinglist.utils.ReadingListTestUtils;
+import modules.user.core.domain.User;
 import modules.user.utils.UserTestUtils;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -12,15 +14,24 @@ import org.junit.jupiter.api.BeforeEach;
 public class InMemoryReadingListRepositoryTest extends AbstractReadingListRepositoryTest {
     
     private InMemoryReadingListRepository inMemoryRepo;
-
+    
     @BeforeEach
-    void setUp() {
+    void setupRepo() {
         inMemoryRepo = new InMemoryReadingListRepository();
-        
-        this.repository = inMemoryRepo;
+    }
+    
+    @Override
+    protected ReadingListRepository getRepository() {
+        return inMemoryRepo;
+    }
 
-        this.testUser1 = UserTestUtils.createValidUser();
-        this.testBook1 = CatalogTestUtils.createValidBook();
-        this.testList1 = this.repository.create(ReadingListTestUtils.createValidReadingListForUser(this.testUser1, "My In-Memory List"));
+    @Override
+    protected User createAndSaveUser() {
+        return UserTestUtils.createValidUser();
+    }
+
+    @Override
+    protected Book createAndSaveBook() {
+        return CatalogTestUtils.createValidBook();
     }
 }
--- src/test/java/modules/readinglist/infrastructure/JpaReadingListRepositoryTest.java
+++ src/test/java/modules/readinglist/infrastructure/JpaReadingListRepositoryTest.java
@@ -2,50 +2,51 @@ package modules.readinglist.infrastructure;
 
 import common.JpaRepositoryTestProfile;
 import common.TransactionalTestHelper;
+import io.quarkus.narayana.jta.QuarkusTransaction;
+import io.quarkus.test.TestTransaction;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 import jakarta.inject.Inject;
+import modules.catalog.core.domain.Book;
 import modules.catalog.utils.CatalogTestUtils;
 import modules.readinglist.core.usecases.repositories.ReadingListRepository;
-import modules.readinglist.utils.ReadingListTestUtils;
+import modules.user.core.domain.User;
 import modules.user.utils.UserTestUtils;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.function.Supplier;
 
 @QuarkusTest
 @TestProfile(JpaRepositoryTestProfile.class)
+@TestTransaction
 public class JpaReadingListRepositoryTest extends AbstractReadingListRepositoryTest {
 
     @Inject
     ReadingListRepository jpaReadingListRepository;
-
+    
     @Inject
     TransactionalTestHelper helper;
 
-    @BeforeEach
-    void setUp() {
-        this.repository = jpaReadingListRepository;
-        
-        this.testUser1 = helper.saveUser(UserTestUtils.createValidUser());
-        this.testBook1 = helper.saveBook(CatalogTestUtils.createValidBook());
-        this.testList1 = helper.saveReadingList(ReadingListTestUtils.createValidReadingListForUser(this.testUser1, "My JPA List"));
+    @Override
+    protected ReadingListRepository getRepository() {
+        return jpaReadingListRepository;
     }
 
-    @AfterEach
-    void tearDown() {
-        var allLists = helper.findReadingListsByUserId(testUser1.getKeycloakUserId());
-        if (allLists != null) {
-             allLists.forEach(list -> helper.deleteReadingList(list.getReadingListId()));
-        }
-        
-        if (testList1 != null) {
-            helper.deleteReadingList(testList1.getReadingListId());
-        }
-        if (testBook1 != null) {
-            helper.deleteBook(testBook1.getBookId());
-        }
-        if (testUser1 != null) {
-            helper.deleteUser(testUser1.getKeycloakUserId());
-        }
+    @Override
+    protected User createAndSaveUser() {
+        return helper.saveUser(UserTestUtils.createValidUser());
+    }
+
+    @Override
+    protected Book createAndSaveBook() {
+        return helper.saveBook(CatalogTestUtils.createValidBook());
+    }
+
+    @Override
+    protected void runTransactionalStep(Runnable step) {
+        QuarkusTransaction.requiringNew().run(step::run);
+    }
+    
+    @Override
+    protected <T> T runTransactionalStep(Supplier<T> step) {
+        return QuarkusTransaction.requiringNew().call(step::get);
     }
 }
