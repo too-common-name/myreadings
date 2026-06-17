88d27060168f4b16f9949f715dad3bb412bf1b87
Daniele Rossi <drossi@redhat.com>
2026-06-17 11:19:58 +0200

    chore: add OTel dependency

--- common-utils/src/main/java/org/modular/playground/common/filters/TraceIdFilter.java
+++ common-utils/src/main/java/org/modular/playground/common/filters/TraceIdFilter.java
@@ -1,5 +1,7 @@
 package org.modular.playground.common.filters;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseContext;
@@ -7,21 +9,25 @@ import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 import org.jboss.logging.MDC;
 import java.io.IOException;
-import java.util.UUID;
 
 @Provider
 public class TraceIdFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     public static final String TRACE_ID_KEY = "traceId";
+    public static final String SPAN_ID_KEY = "spanId";
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        String traceId = UUID.randomUUID().toString();
-        MDC.put(TRACE_ID_KEY, traceId);
+        SpanContext ctx = Span.current().getSpanContext();
+        if (ctx.isValid()) {
+            MDC.put(TRACE_ID_KEY, ctx.getTraceId());
+            MDC.put(SPAN_ID_KEY, ctx.getSpanId());
+        }
     }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         MDC.remove(TRACE_ID_KEY);
+        MDC.remove(SPAN_ID_KEY);
     }
 }
